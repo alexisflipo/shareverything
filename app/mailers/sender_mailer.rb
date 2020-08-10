cc9f23df6ec40c51562346f49dcab10db89ac133
Alexis Flipo <alexis.flipo1@gmail.com>
2020-08-10 22:46:59 +0200

    Refacto

--- app/mailers/sender_mailer.rb
+++ app/mailers/sender_mailer.rb
@@ -2,7 +2,6 @@ class SenderMailer < ApplicationMailer
 
   def send_to_sender(sender)
     @sender = sender
-
     mail to: @sender.email,
     subject: "Sending confirmed"
   end
@@ -10,7 +9,7 @@ class SenderMailer < ApplicationMailer
   def send_to_recipient(sender)
     @sender = sender
     mail to: @sender.recipient,
-          subject: "#{@sender.email} sent you files via Shareverything"
+    subject: "#{@sender.email} sent you files via Shareverything"
   end
 
 end
