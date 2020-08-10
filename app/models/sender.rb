6ab2e484b2e4da03270b1bef0ccdc471276c3c8e
Jules Prevost <jprevostdev@gmail.com>
2020-08-10 17:55:58 +0200

    Add custom email validations in Sender model. No DB save is necessary

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,2 +1,8 @@
-class Sender < ApplicationRecord
+class Sender
+  include ActiveModel::Model
+
+  attr_accessor :email, :receiver_email, :message, :username
+
+  validates :email, presence: true, format: { with: /\A.+\@.+\.[a-z]{1,4}\z/}
+  validates :receiver_email, presence: true, format: { with: /\A.+\@.+\.[a-z]{1,4}\z/}
 end
