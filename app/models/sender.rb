7fa737dc9c4b42f3c67435404fb0766b3702604c
Jules Prevost <jprevostdev@gmail.com>
2020-08-11 08:10:18 +0200

    Add File to the form. It works. You can now send docs to cloudinary through the form. I have saved the public_id of cloudinary file in file->random_code.

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,7 +1,7 @@
 class Sender
   include ActiveModel::Model
 
-  attr_accessor :email, :recipient, :message, :username
+  attr_accessor :email, :recipient, :message, :username, :file
 
   validates :email, presence: true, format: { with: /\A.+\@.+\.[a-z]{1,4}\z/}
   validates :recipient, presence: true, format: { with: /\A.+\@.+\.[a-z]{1,4}\z/}
