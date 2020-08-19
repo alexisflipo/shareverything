a53a4aeb67b519e44748b71cfd2042a103cefd3c
Alexis Flipo <alexis.flipo1@gmail.com>
2020-08-19 16:45:00 +0200

    gmail prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['GMAIL'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
