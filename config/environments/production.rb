bdc1a5a20220272e871809dccab0452e2bdb8717
Jules Prevost <jprevostdev@gmail.com>
2020-08-19 17:02:53 +0200

    config mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { :host =>'shareverything.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV['GMAIL'],
     :password => ENV['GMAIL_PASSWORD'],
     :address => 'smtp.gmail.com',
     :port => 587,
+    :domain => "gmail.com",
     :authentication => :plain,
     :enable_starttls_auto => true
   }
