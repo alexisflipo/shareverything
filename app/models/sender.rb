af97776c3a92682260068ecfdd3052afc6028baf
Alexis Flipo <alexis.flipo1@gmail.com>
2020-08-20 11:59:36 +0200

    Limite à 4MB l'upload de fichiers + bouton réparé

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -3,6 +3,8 @@ class Sender
 
   attr_accessor :email, :recipient, :message, :username, :url, :file
 
-  validates :email, presence: true, format: { with: /\A.+\@.+\.[a-z]{1,4}\z/, message: "nique ta mère"}
+  validates :email, presence: true, format: { with: /\A.+\@.+\.[a-z]{1,4}\z/}
   validates :recipient, presence: true, format: { with: /\A.+\@.+\.[a-z]{1,4}\z/}
+  validates :file, presence: true, file_size: { less_than: 4.megabytes,
+    message: "must not exceed 4MB. Please choose a smaller file." }
 end
