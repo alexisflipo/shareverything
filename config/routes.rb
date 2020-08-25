f34f3de4dc3886006845a229ba33c60e225be6bd
Alexis Flipo <alexis.flipo1@gmail.com>
2020-08-25 11:26:05 +0200

    Ajout d'authorisation pour sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   require "sidekiq/web"
-
-  mount Sidekiq::Web => '/sidekiq'
-
+  authenticate :user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :senders, only: [:new, :create]
