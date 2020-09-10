a493328f099e98af0f441b12dc44025acb9e8968
ManyBaby <emmanuel@skycitizen.fr>
2020-09-10 12:03:38 +0200

    Fine tuning the controller

--- app/controllers/senders_controller.rb
+++ app/controllers/senders_controller.rb
@@ -10,7 +10,7 @@ class SendersController < ApplicationController
     @sender = Sender.new(sender_params)
     if @sender.save
       @document = Document.new
-      @document.file.attach(document_params)
+      @document.file.attach(document_params[:file])
       @document.save!
       @sender.url = @document.url
       SendJob.perform_now(@sender)
@@ -33,13 +33,11 @@ class SendersController < ApplicationController
     params.require(:sender).permit(:message, :email, :recipient, :username, :days)
   end
 
-
   def document_params
-    params.require(:sender).permit(:file)
+    params.require(:document).permit(:file)
   end
 
   def redirect_root
     redirect_to root_path, notice: "Message and file sent successfully. A confirmation has been sent on your email"
   end
-
 end
