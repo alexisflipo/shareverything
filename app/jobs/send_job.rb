bf81c4c10075c8114d222f97fb1a6022fa63212d
Jules Prevost <jprevostdev@gmail.com>
2020-09-10 09:56:49 +0200

    Passing id to SendJob instead of instance

--- app/jobs/send_job.rb
+++ app/jobs/send_job.rb
@@ -1,7 +1,8 @@
 class SendJob < ApplicationJob
   queue_as :default
 
-  def perform(sender)
+  def perform(sender_id)
+    sender = Sender.find(sender_id)
     SenderMailer.send_to_sender(sender).deliver
     SenderMailer.send_to_recipient(sender).deliver
   end
