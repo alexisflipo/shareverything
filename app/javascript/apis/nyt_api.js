eb6dea7e2888a4e8c72d725ee9d1bc18101954d6
Jules Prevost <jprevostdev@gmail.com>
2020-09-11 10:19:54 +0200

    JS works, our website take the 4 first articles actually in Home of the NYT and display them on our home page

--- app/javascript/apis/nyt_api.js
+++ app/javascript/apis/nyt_api.js
@@ -6,7 +6,13 @@ const nyt = document.getElementById("nyt");
      fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=dlhLwAV19TVntyHf796mfDOXhXA0xkJH")
      .then(response => response.json())
      .then((data) => {
-       const firtsFourResults = data["results"].slice(0, 4);
+      const firtsFourResults = data["results"].slice(0, 4);
+      const title = `
+        <h3>Some time ? Read the New-York-Times !</h3>
+        <div class="card-articles" id="nyt-articles">
+        </div>
+      `
+      nyt.insertAdjacentHTML("beforeend", title);
        firtsFourResults.forEach((article) => {
           const infos = {
             title: article['title'],
@@ -24,7 +30,21 @@ const nyt = document.getElementById("nyt");
 
 
  const displayArticlesInfos = (infos) => {
-  const infosHtml = ``
+  const nytArticles = document.getElementById("nyt-articles");
+  const infosHtml = `
+    <div class="card-article">
+      <a href="${infos['url']}" target="_blank">
+        <img src="${infos['imgUrl']}" alt="Image illustrating the article ${infos['title']}">
+        <div class="card-article-infos">
+          <p class="copyright">© ${infos['imgCopyright']}</p>
+          <h4>${infos['title']}</h4>
+          <p>${infos['summary']}</p>
+        </div>
+      </a>
+    </div>
+  `
+
+  nytArticles.insertAdjacentHTML("beforeend", infosHtml);
  };
 
  export { callNytApi };
